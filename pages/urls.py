139d1e1dbff5f93ea1740e725f048ab82f66eb41
Shah Wajahat Ejaz <shah.wajahat@systemsltd.com>
2022-08-17 00:43:46 +0500

    creating three pages about,service and contact

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,12 @@
+from unicodedata import name
 from pages.views import home
 from django import views
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),
+    path('',views.home, name='home'),
+    path('about',views.about,name='about'),
+    path('contact',views.contact,name='contact'),
+    path('service',views.contact,name='service'),
 ]
